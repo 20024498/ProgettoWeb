0557786fc799686323e48f1bff042e82a0363fa1
20024498 <20024498@studenti.uniupo.it>
2019-07-26 13:34:35 +0200

    insertion ok

--- Autenticazione.php
+++ Autenticazione.php
@@ -1,28 +1,35 @@
 <?php
 
-include "Database.php";
+$hostName = "localhost";
+$userName = "pweb1819gilia";
+$password = "GmDMs7hFnPFN";
+$database = "my_pweb1819gilia";
+
+$db_connection = new mysqli($hostname, $username, $password, $database);
+
+if ($db_connection->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
+   echo "connessione stabilità";
+}
+
 
 $nome = $_POST["nome"];
 $cognome = $_POST["cognome"];
-$mail = $_POST["email"];
+$mail = $_POST["mail"];
 
 
-$db = $db_connection;
+$sql = "INSERT INTO Credenziali (Nome, Cognome, Email)
+VALUES ('$nome', '$cognome', '$mail')";
 
-$db->query("INSERT INTO Credenziali (Nome,Cognome,Email) VALUES ($nome,$cognome,$mail)");
+if (mysqli_query($db_connection, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($db_connection);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+mysqli_close($db_connection);
 
 
 
