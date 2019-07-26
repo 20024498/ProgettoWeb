9df6d1e483f3911bf9f1cb4ce44e2b8bfeb1e24b
20024498 <20024498@studenti.uniupo.it>
2019-07-26 10:14:24 +0200

    responsive registrati non finito

--- Autenticazione.php
+++ Autenticazione.php
@@ -7,8 +7,9 @@ $cognome = $_POST["cognome"];
 $mail = $_POST["email"];
 
 
+$db = $db_connection;
 
-
+$db->query("INSERT INTO Credenziali (Nome,Cognome,Email) VALUES ($nome,$cognome,$mail)");
 
 
 
