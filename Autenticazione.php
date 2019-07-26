ead514c436bb9f20714236991ed8878eb97f3db2
20024498 <20024498@studenti.uniupo.it>
2019-07-26 14:35:06 +0200

    css commit

--- Autenticazione.php
+++ Autenticazione.php
@@ -1,19 +1,6 @@
 <?php
-
-$hostName = "localhost";
-$userName = "pweb1819gilia";
-$password = "GmDMs7hFnPFN";
-$database = "my_pweb1819gilia";
-
-$db_connection = new mysqli($hostname, $username, $password, $database);
-
-if ($db_connection->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-else{
-   echo "connessione stabilità";
-}
-
+ 
+include 'Connessione.php';
 
 $nome = $_POST["nome"];
 $cognome = $_POST["cognome"];
@@ -30,7 +17,6 @@ if (mysqli_query($db_connection, $sql)) {
 }
 
 mysqli_close($db_connection);
-
 
 
 
